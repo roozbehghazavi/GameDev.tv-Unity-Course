f86e963af9e9ed0efc542ff3497a337d607965c0
roozbehghazavi <roozbehghazavi3@gmail.com>
2022-01-21 19:16:50 +0330

    PB_2

---
+++ Unity Projects/Unity3D/3_Project_Boost/Project Boost/Assets/Rocket.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            print("Thrusting");
+        }    
+        if(Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating right");
+        }
+    }
+}
