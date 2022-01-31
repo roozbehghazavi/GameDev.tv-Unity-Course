358203b3a236d223a31bc55a87ccb9d0b7b44701
roozbehghazavi <roozbehghazavi3@gmail.com>
2022-01-31 19:09:26 +0330

    rigidbody moving

--- Unity Projects/Unity3D/3_Project_Boost/Project Boost/Assets/Scripts/Rocket.cs
+++ Unity Projects/Unity3D/3_Project_Boost/Project Boost/Assets/Scripts/Rocket.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidbody; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,17 +20,17 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-            print("Thrusting");
-        }    
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
         if(Input.GetKey(KeyCode.A))
         {
-            print("Rotating left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
