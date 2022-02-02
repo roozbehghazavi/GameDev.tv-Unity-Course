7915addc4e2aadd5eb7fd3196545b2cd7cb77405
roozbehghazavi <roozbehghazavi3@gmail.com>
2022-02-02 18:44:06 +0330

    refactoring

--- Unity Projects/Unity3D/3_Project_Boost/Project Boost/Assets/Scripts/Rocket.cs
+++ Unity Projects/Unity3D/3_Project_Boost/Project Boost/Assets/Scripts/Rocket.cs
@@ -17,10 +17,10 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput(); 
+        Thrust();
+        Rotate(); 
     }
-
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
@@ -35,8 +35,12 @@ public class Rocket : MonoBehaviour
         {
             audioSource.Stop();
         }
+    }
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true;
 
-        if(Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
         }
@@ -44,5 +48,9 @@ public class Rocket : MonoBehaviour
         {
             transform.Rotate(-Vector3.forward);
         }
+
+        rigidbody.freezeRotation = false;
     }
+
+
 }
